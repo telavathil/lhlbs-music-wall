8c972f55cfb7b7b178340245814c8ce7d49d8c6b
Tobin Elavathil <tobin.elavathil@gmail.com>
2016-07-12 21:34:18 -0400

    reviews skeleton up, functionality not working

--- app/actions.rb
+++ app/actions.rb
@@ -1,47 +1,47 @@
 # Homepage (Root path)
 get '/' do
-  erb :index
+    erb :index
 end
 
 get '/tracks' do
-  @tracks = Track.all
-  erb:'tracks/index'
+    @tracks = Track.all
+    erb:'tracks/index'
 end
 
 get '/tracks/new' do
-  # @track = Track.new
-  erb:'tracks/new'
+    # @track = Track.new
+    erb:'tracks/new'
 end
 
 post '/tracks' do
-  #binding.pry
-  #create google link from song title
-  params[:url] = "https://play.google.com/store/search?q=#{params[:song_title].gsub(/\s/,'+')}&c=music"
+    # binding.pry
+    # create google link from song title
+    params[:url] = "https://play.google.com/store/search?q=#{params[:song_title].gsub(/\s/, '+')}&c=music"
 
-  @track = Track.new(
-  song_title: params[:song_title],
-  author: params[:author],
-  url: params[:url],
-  user_id: params[:user_id]
-  )
-  if @track.save
-    redirect 'tracks'
-  else
-    erb:'/tracks/new'
-  end
+    @track = Track.new(
+        song_title: params[:song_title],
+        author: params[:author],
+        url: params[:url],
+        user_id: params[:user_id]
+    )
+    if @track.save
+        redirect 'tracks'
+    else
+        erb:'/tracks/new'
+    end
 end
 
 get '/tracks/:id' do
-  @track = Track.find(params[:id])
-  erb:'tracks/show'
+    @track = Track.find(params[:id])
+    erb:'tracks/show'
 end
 
 get '/signup' do
-  erb:'signup/index'
+    erb:'signup/index'
 end
 
 get '/signin' do
-  erb:'signin/index'
+    erb:'signin/index'
 end
 
 # get '/signup/oops' do
@@ -50,55 +50,60 @@ end
 # end
 
 post '/signup' do
-  @user = User.new(
-  email: params[:email],
-  password: params[:password]
-  )
-  if @user.save
-    session[:user_id] = @user.id
-    redirect '/users'
-  else
-    # binding.pry
-    redirect '/signup'
-  end
+    @user = User.new(
+        email: params[:email],
+        password: params[:password]
+    )
+    if @user.save
+        session[:user_id] = @user.id
+        redirect '/users'
+    else
+        # binding.pry
+        redirect '/signup'
+    end
 end
 
 post '/signin' do
-  #binding.pry
-  if user = User.find_by(params[:id])
-    session[:user_id] = user.id
-    redirect '/'
-  else
-    @error = "Invalid User"
-    erb :'index'
-  end
+    # binding.pry
+    if user = User.find_by(params[:id])
+        session[:user_id] = user.id
+        redirect '/'
+    else
+        @error = 'Invalid User'
+        erb :index
+    end
 end
 
-
 get '/users' do
-  if session[:user_id]
-  @users = User.all
-  erb:'users/index'
-  else
-    redirect '/signin'
-  end
+    if session[:user_id]
+        @users = User.all
+        erb:'users/index'
+    else
+        redirect '/signin'
+    end
 end
 
 get '/logout' do
-  session[:user_id] = nil
-  redirect '/'
+    session[:user_id] = nil
+    redirect '/'
+end
+
+post '/vote_up_track' do
+    Upvote.create(user_id: session[:user_id], track_id: params[:track_id]) unless Upvote.find_by(track_id: params[:track_id], user_id: session[:user_id])
+    redirect 'tracks'
+end
+
+post '/add_review' do
+    Review.create(user_id: session[:user_id], track_id: params[:track_id], review: params[:review]) unless Review.find_by(track_id: params[:track_id], user_id: session[:user_id], review: params[:review])
+    binding.pry
+    redirect "tracks/#{params[:track_id]}"
 end
 
 helpers do
-  def current_user
-    if session[:user_id]
-      # @current_user ||= User.find(session[:id])
-      return User.find(session[:user_id])
+    def current_user
+        if session[:user_id]
+            # @current_user ||= User.find(session[:id])
+            return User.find(session[:user_id])
+        end
     end
-  end
-
-  post '/vote_up_track' do
-    Upvote.create(user_id:session[:user_id],track_id: params[:track_id]) unless Upvote.find_by(track_id: params[:track_id], user_id: session[:user_id])
-    redirect 'tracks'
-  end
 end
